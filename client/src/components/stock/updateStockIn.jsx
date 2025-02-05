00a9a6bf1d3a259d81768eb11fbcecbaa6c7378a
Rajdeep <121rajpaul@gmail.com>
2025-02-05 22:57:56 +0530

    Rename handleSubmitStockIn to handleUpdateStockIn for clarity in the UpdateStockIn component

--- client/src/components/stock/updateStockIn.jsx
+++ client/src/components/stock/updateStockIn.jsx
@@ -241,7 +241,7 @@ const UpdateStockIn = () => {
   };
 
   //final submit of stock in
-  const handleSubmitStockIn = async () => {
+  const handleUpdateStockIn = async () => {
 
     let products = [];
     const stockInFormdata = new FormData();
@@ -787,7 +787,7 @@ const UpdateStockIn = () => {
           <div class="flex justify-end">
             <button
               type="button"
-              onClick={() => handleSubmitStockIn()}
+              onClick={() => handleUpdateStockIn()}
               class="text-white  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-semibold rounded-lg text-lg w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               Update
